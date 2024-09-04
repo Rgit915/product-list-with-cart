0bdaee76dc8d58fe5e24b388124c26ca022d3a04
Rgit915 <rora.goitom@gmail.com>
2024-09-04 10:38:08 -0400

    Add styling to Desserts page

--- app/ui/Desserts.jsx
+++ app/ui/Desserts.jsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 import images from "../lib/data.json";
 
 export default function DessertPage() {
+  
   return (
     <section className="dessert-page ">
-      <div className="heading">
-        <h2>
+      <div className="heading my-4">
+        <h2 className="text-5xl text-rose-900 font-black leading-10">
           Desserts
         </h2>
       </div>
-      <div className="dessert-menu">
+      <div className="dessert-menu md:grid md:grid-cols-3 md:gap-8">
         {images.map((item, index) => (
           <div className="dessert-item relative" key={index}>
             <div className="dessert-image-container ">
@@ -35,6 +36,8 @@ export default function DessertPage() {
                   alt={`${item.name} image`}
                   width={250}
                   height={240}
+
+                  className="rounded-xl object-cover w-full h-auto"
                 />
               </picture>
               {/* <Image
@@ -54,8 +57,8 @@ export default function DessertPage() {
               className="rounded-xl object-cover w-full h-auto"
             /> */}
             </div>
-            <div className="button-container ">
-              <button>
+            <div className="button-container w-[160x] h-[44px] absolute top-[60%] left-[20%]  flex justify-center space-x-6 ">
+              <button className=" button rounded-3xl border-2 border-rose-400 px-4 py-2 bg-white flex justify-between items-center gap-4">
                 <Image
                   src="/assets/images/icon-add-to-cart.svg"
                   alt="image cart icon"
@@ -66,14 +69,14 @@ export default function DessertPage() {
                 Add to Cart
               </button>
             </div>
-            <div className="dessert-details">
-              <span className="dessert-category">
+            <div className="dessert-details my-8">
+              <span className="dessert-category text-rose-500 font-normal leading-4 ">
                 {item.category}
               </span>
-              <p className="dessert-name">
+              <p className="dessert-name font-semibold text-rose-900 leading-5">
                 {item.name}
               </p>
-              <div className="dessert-price">
+              <div className="dessert-price text-red leading-6 font-semibold">
                 ${item.price}
               </div>
             </div>
