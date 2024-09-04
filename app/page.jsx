e1ec5e5706ee32bac6256f7d2ae1d6602d51106a
Rgit915 <rora.goitom@gmail.com>
2024-09-04 10:18:21 -0400

    Add Home page

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import DessertPage from "./ui/Desserts";
+import Cart from "./ui/Cart";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center p-8 md:w-full lg:flex-row lg:items-start lg:justify-between">
+      <DessertPage />
+      <Cart/>
+    </main>
+  );
+}
