55a0776eecd449f43d329cf6e99ea442465756ae
Rgit915 <rora.goitom@gmail.com>
2024-09-11 09:36:42 -0400

    Add styles to Cart component

--- app/ui/Cart.jsx
+++ app/ui/Cart.jsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 
-export default function Cart(){
-
-  return(
+export default function Cart() {
+  return (
     <section className="cart-page">
-      <div>
-      <h2>Your Cart (0) </h2>
-      <Image src="/assets/images/illustration-empty-cart.svg" alt="image icon of cake"
-      width={128} height={128} />
-      <p>Your added items will appear here.</p>
+      <div className="bg-white rounded-lg p-4 w-full">
+        <h2 className="text-3xl font-bold text-red">Your Cart (0) </h2>
+        <div className="flex flex-col items-center justify-between gap-4">
+          <Image
+            src="/assets/images/illustration-empty-cart.svg"
+            alt="image icon of cake"
+            width={128}
+            height={128}
+          />
+          <p className="text-md leading-4 text-rose-500 font-semibold ">
+            Your added items will appear here.
+          </p>
+        </div>
       </div>
     </section>
   );
