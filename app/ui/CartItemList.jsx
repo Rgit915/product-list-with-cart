c8a45666d760b7d8ab3d4fa62ae144b01ad3988d
Rgit915 <rora.goitom@gmail.com>
2024-09-12 16:52:12 -0400

    Calculate the order total

--- app/ui/CartItemList.jsx
+++ app/ui/CartItemList.jsx
@@ -3,23 +3,32 @@ import Image from "next/image";
 
 const CartItemList = () => {
   const { cartItems, removeFromCart } = useCart();
+
+  // Calculate the order total
+  const orderTotal = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+  
   return (
     <div className="cart-items-list">
       <ul className="cart-items list-none">
         {cartItems.map((item, index) => (
           <li
             key={index}
-            className="flex justify-between items-center gap-4 py-2"
+            className="flex flex-col justify-between items-center gap-4 py-2"
           >
             <div className="flex justify-between w-full py-4 text-sm leading-[18.52px]">
               <div className="space-y-2">
-                <p className=" text-rose-900 font-semibold">{item.name}</p>
-                <p className=" text-rose-400 font-semibold ">
-                  <span className="item-qauntity text-red font-semibold pr-4">
-                    1x
+                <p className="text-rose-900 font-semibold">{item.name}</p>
+                <p className="text-rose-400 font-semibold">
+                  <span className="item-quantity text-red font-semibold pr-4">
+                    {item.quantity}x
                   </span>
                   @ ${item.price}
-                  <span className="total-price pl-2">${item.price}</span>
+                  <span className="total-price pl-2">
+                    ${(item.price * item.quantity).toFixed(2)}
+                  </span>
                 </p>
               </div>
 
@@ -29,15 +38,37 @@ const CartItemList = () => {
                   alt="remove button Icon"
                   width={17.5}
                   height={17.5}
-                  className="rounded-full border-2 border-rose-400 "
-                  style={{ width: 'auto', height: 'auto' }}
+                  className="rounded-full border-2 border-rose-400 px-1 py-1"
+                  style={{ width: "auto", height: "auto" }}
                 />
               </button>
             </div>
-            <hr />
+            <hr className="text-rose-100 w-full " />
           </li>
         ))}
       </ul>
+      <div className="flex justify-between w-full py-4 text-sm leading-[18.52px]">
+        <div className="flex justify-between items-center gap-4 w-full">
+          <p className="text-sm text-rose-900 font-normal">Order Total</p>
+          <p className="text-2xl text-rose-900 font-black">${orderTotal.toFixed(2)}</p>
+        </div>
+      </div>
+      <div className="carbon-neutral flex justify-between items-center bg-rose-50 rounded-lg px-4 py-4 mb-4 md:justify-center md:gap-2 ">
+        <Image
+          src="/assets/images/icon-carbon-neutral.svg"
+          alt="image cart icon"
+          width={20}
+          height={20}
+          className="w-5 h-auto"
+        />
+        <p className="text-sm text-rose-900 font-normal">
+          This is a <span className="font-semibold">carbon-neutral</span>{" "}
+          delivery
+        </p>
+      </div>
+      <button className="button w-full py-4 text-base font-semibold text-white bg-red rounded-3xl">
+        Confirm Order
+      </button>
     </div>
   );
 };
