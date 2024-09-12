bfb4bfc2717105b597e49e23dbc4b37274fa218c
Rgit915 <rora.goitom@gmail.com>
2024-09-12 09:18:14 -0400

    Add bg color to homePage

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Cart from "./ui/Cart";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center p-8 md:w-full lg:flex-row lg:items-start lg:justify-between font-Red_Hat_Text">
+    <main className="flex min-h-screen flex-col justify-center p-8 md:w-full lg:flex-row lg:items-start lg:justify-between font-Red_Hat_Text bg-rose-50">
       <DessertPage />
       <Cart/>
     </main>
